96fde36fcfcf22fee99889b72a259b583b495698
alom310 <alom310@gmail.com>
2019-04-30 13:52:26 -0700

    phpmailer

--- contactform.php
+++ contactform.php
@@ -1,16 +1,34 @@
 <?php
 
+$result="";
+
 if (isset($_POST['submit'])) {
-    $name = $_post['name'];
-    $subject = $_post['subject'];
-    $mailFrom = $_post['mail'];
-    $memssage = $_post['message'];
+    require 'PHPMailer/PHPMailerAutoload.php';
 
-    $mailTo = "alom310@gmail.com";
-    $headers = "From: ".$mailFrom;
-    $txt = "You have received an e-mail from ".$name.".\n\n".$message;
+    $mail = new PHPMailer;
 
-    mail($mailTo, $subject, $txt, $headers);
+    $mail->Host='smtp.gmail.com';
+    $mail->PORT=587;
+    $mail->SMTPAuth=true;
+    $mail->SMTPSecure='tls';
+    $mail->Username='alom310@gmail.com';
+    $mail->Password='Rigmola1234!';
 
-    header("Location: index.php?mailsend");
+    $mail->setFrom($_POST['email'],$_POST['name']);
+    $mail->addAddress('alom310@gmail.com');
+
+    $mail->isHTML(true);
+    $mail->Subject='Form Submission: '.$_POST['subject'];
+    $mail->Body='<h1 align=center>Name :'.$_POST['name'].'<br>Email: '.$_POST['email'].'<br>Message: '.$_POST['msg'].'<h1>';
+
+
+    if(!$mail->send()){
+        $result="Something went wrong. Please try again.";
+    }
+    else{
+        $result="Thanks ".$_POST['name']." for getting in touch with. I'll get back to you soon!";
+    }
+
 }
+
+?>
