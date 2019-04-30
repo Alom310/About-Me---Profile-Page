96f9a55dd01f93849224e0437f5dbe6fe16bec46
alom310 <alom310@gmail.com>
2019-04-30 14:17:26 -0700

    php form

--- index.php
+++ index.php
@@ -1,4 +1,37 @@
-<!doctype html>
+<!-- <!doctype html> -->
+<?php
+  $result="";
+
+  if (isset($_POST['submit'])) {
+      require 'PHPMailer/PHPMailerAutoload.php';
+
+      $mail = new PHPMailer;
+
+      $mail->Host='smtp.gmail.com';
+      $mail->PORT=587;
+      $mail->SMTPAuth=true;
+      $mail->SMTPSecure='tls';
+      $mail->Username='vandaleyindus@gmail.com';
+      $mail->Password='123456Sf';
+
+      $mail->setFrom($_POST['email'],$_POST['name']);
+      $mail->addAddress('vandaleyindus@gmail.com');
+
+      $mail->isHTML(true);
+      $mail->Subject='Form Submission: '.$_POST['subject'];
+      $mail->Body='<h1 align=center>Name :'.$_POST['name'].'<br>Email: '.$_POST['email'].'<br>Message: '.$_POST['msg'].'<h1>';
+
+
+      if(!$mail->send()){
+          $result="Something went wrong. Please try again.";
+      }
+      else{
+          $result="Thanks ".$_POST['name']." for getting in touch with. I'll get back to you soon!";
+      }
+
+  }
+?>
+
 <html class="no-js" lang="">
     <head>
         <meta charset="utf-8">
@@ -147,8 +180,8 @@
     <div style="height:20px;" id="contact"></div>
     <section>
         <h2 class="mt-3">Get in touch</h2>
-        <form action="contactform.php" method="post" enctype="text/plain">
-          <h5>?= $result; ?</h5>
+        <h5>?= $result; ?</h5>
+        <form action="index.php" method="post" enctype="text/plain">
           <input type="text" name="name" placeholder="Full name" required>
           <input type="text" name="mail" placeholder="Your e-mail" required>
           <input type="text" name="subject" placeholder="Subject" required>
